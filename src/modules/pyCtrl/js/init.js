4b83a2c4e57340227c8331f0b50039febdc2460a
Eddy Hintze <eddy.hintze@gmail.com>
2014-10-27 19:05:19 -0400

    Added context menu for script actions

--- src/modules/pyCtrl/js/init.js
+++ src/modules/pyCtrl/js/init.js
@@ -4,16 +4,11 @@ var sendCmdUrl = "./modules/pyCtrl/getScriptData.php";
 
 var Script = {
     _template: _.template(
-        '<div id="script-{{name}}">' +
-        '   <div id="script-{{name}}-dialog" class="dialog" title="Output"></div>' +
+        '<div id="script-{{name}}" class="context-menu-pyCtrl-scripts">' +
+        '   <div id="script-{{name}}-dialog" class="dialog" title="{{name}}"></div>' +
         '   <span class="script-status"></span>' +
         '   <span class="script-name">{{name}}</span>' +
         '   <span class="script-uptime">{{uptime}}</span>' +
-        '   <button data-cmd="/script/{{name}}/enable">Enable</button>' +
-        '   <button data-cmd="/script/{{name}}/disable">Disable</button>' +
-        '   <button data-cmd="/script/{{name}}/run">Run</button>' +
-        '   <button data-cmd="/script/{{name}}/stop">Stop</button>' +
-        '   <button class="js-script-output-live" data-cmd="/script/{{name}}/output/live">Output/Live</button>' +
         '</div>'
     ),
     dialogIntervals: {},
@@ -22,9 +17,9 @@ var Script = {
             _.each(scripts.all, function(script){
                 $('#scriptsContainer').append(Script._template(script));
                 var $scriptBox = $("#script-"+script.name);
-                $scriptBox.find('button').on('click',function(e){
-                    Script.cmd(e, script.name);
-                });
+                //$scriptBox.find('button').on('click',function(e){
+                //    Script.cmd(e, script.name);
+                //});
                 $("#script-"+script.name+"-dialog").dialog({
                     autoOpen: false,
                     close: function( event, ui ){
@@ -61,10 +56,8 @@ var Script = {
             });
         })
     },
-    cmd: function(e, name){
-        var $target = $(e.currentTarget);
-        var cmd_ = $target.data('cmd');
-        if($target.hasClass('js-script-output-live')){
+    cmd: function(cmd_, name){
+        if(cmd_ == '/script/'+name+'/output/live'){
             var $dialogBox = $("#script-"+name+"-dialog");
             if(typeof Script.dialogIntervals[name] === 'undefined'){
                 $dialogBox.dialog("open");
@@ -81,19 +74,36 @@ var Script = {
                 window.clearInterval(Script.dialogIntervals[name]);
                 Script.dialogIntervals[name] = undefined;
             }
-        }else {
+        }else{
             Script.sendCommand(cmd_, function (response) {
                 $("#scriptResponse").html(response.output);
             });
         }
-
-
-
     }
 }; //END var Script
-
 
 
 Script.init();
 
 
+$.contextMenu({
+    selector: '.context-menu-pyCtrl-scripts',
+    trigger: 'left',
+    build: function($trigger, e){
+        var $scriptName = $trigger.find(".script-name").html();
+        return {
+            callback: function (key, options) {
+                var cmd = '/script/'+$scriptName+'/'+key;
+                Script.cmd(cmd, $scriptName);
+            }
+        };
+    },
+    items: {
+        "enable": {name: "Enable"},
+        "disable": {name: "Disable"},
+        "run": {name: "Run"},
+        "stop": {name: "Stop"},
+        "sep1": "---------",
+        "output/live": {name: "Live Output"}
+    }
+});
